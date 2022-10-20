29bab8717c2b1f193764cbe384b6430d6b93e815
ruslanramong <s65309010017@ptnicec.ac.th>
2022-10-20 18:06:34 +0700

    ส่ง test.php ครั้งที่1

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+
+echo"ruslan";
+
+?>
+<html>
+    <title>11</title>
+    <head></head>
+    <body><br>information<br>
+    <?php
+    echo "pattani";
+    ?>
+</body>
+</html>
+<br>
+<?php
+echo "thailand";
+?>
